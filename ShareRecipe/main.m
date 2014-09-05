cd3694e18c2e104596d7d88277c2108248e34ad7
Jong <jonglee@berkeley.edu>
2014-09-05 16:12:03 -0700

    shareRecipe in progress

---
+++ ShareRecipe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShareRecipe
+//
+//  Created by Jong Yun Lee on 9/2/14.
+//  Copyright (c) 2014 HeapStack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ShareRecipeTests/ShareRecipeTests.m
@@ -0,0 +1,34 @@
+//
+//  ShareRecipeTests.m
+//  ShareRecipeTests
+//
+//  Created by Jong Yun Lee on 9/2/14.
+//  Copyright (c) 2014 HeapStack. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShareRecipeTests : XCTestCase
+
+@end
+
+@implementation ShareRecipeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
